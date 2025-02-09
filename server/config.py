38210acf4f27732363fad8852da11c5d981edf53
Daniel Oshima <doshima2013@gmail.com>
2025-02-09 15:06:44 -0500

    update config.py and write register a new user route

--- server/config.py
+++ server/config.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy import MetaData
+from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///music_app.db"
