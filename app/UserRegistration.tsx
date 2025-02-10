b7c6e186011367cc4bd0bee7f8ee18225195dad1
Daniel Oshima <doshima2013@gmail.com>
2025-02-10 16:22:34 -0500

    add userlogin.tsx file with login component and update user registration component

--- app/UserRegistration.tsx
+++ app/UserRegistration.tsx
@@ -1,6 +1,6 @@
 import {useState} from "react";
 
-function UserRegistration() {
+function Registration() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
@@ -43,11 +43,11 @@ function UserRegistration() {
                     <h1>User Registration Form</h1>
                     <form onSubmit={handleSubmit}>
                         <div>
-                            <label htmlFor="username">Username:</label>
+                            <label htmlFor="username">Enter A Username:</label>
                             <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)}></input>
                         </div>
                         <div>
-                            <label htmlFor="password">Password:</label>
+                            <label htmlFor="password">Enter A Password:</label>
                             <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
                         </div>
                         <button type="submit">Click Here To Register</button>
