5a6b0c183832f3e09f0d034e754fa0079ce15f60
Ingeborg Adolfs <ingeborgadolfs@gmail.com>
2025-02-10 23:55:43 -0700

    add relationships

--- server/config.py
+++ server/config.py
@@ -2,8 +2,9 @@ from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy.ext.associationproxy import association_proxy
-from sqlalchemy import MetaData
+## from sqlalchemy import MetaData
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy_serializer import SerializerMixin
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///music_app.db"
